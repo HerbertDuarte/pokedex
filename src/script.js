097306602102c2f96a770a47b10d8182a1a40bf5
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-25 23:48:20 -0300

    add search function

--- src/script.js
+++ src/script.js
@@ -1,62 +1,103 @@
-var main = document.getElementById('main')
+const main = document.getElementById('main')
+const input = document.querySelector('input#search')
+var arrayPokemons = [{}]
 
 async function fetchPokemon(pokemon){
   const data = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)).json()
-  const img = await fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`)
-
+  const img = data.sprites.front_default
   const name = data.name[0].toUpperCase() + data.name.slice(1)
-  const urlImg = img.url
   const type0 = data.types[0].type.name
   const pokeId = data.id
-
-  if(data.types[1]){
-    const type1 = data.types[1].type.name
   
-    main.innerHTML += (
-      `<div class="pokeCard card${type0}">
-      <div class="infos">
-      <p>${pokeId}. ${name}</p>
-        <img src="${urlImg}" alt="${name}">
-      </div>
-      <div class="divTypes">
-      <span class="type ${type0}">${type0}</span>
-      <span class="type ${type1}">${type1}</span>
-      </div>
-      </div>`
-      )
+  arrayPokemons.splice(data.id , 1, {
+    id: data.id,
+    name: data.name,
+    img : data.sprites.front_default,
+    type0: data.types[0].type.name,
+    type1: undefined,
+    gerarType1(){
+      if(data.types[1]){
+        this.type1 = data.types[1].type.name
+      }
+    }
+  })
 
-  }else{
-    main.innerHTML +=(
-      `<div id="app" class="pokeCard card${type0}">
-      <div class="infos">
-      <p>${pokeId}. ${name}</p>
-        <img src="${urlImg}" alt="${name}">
-      </div>
-      <div class="divTypes">
-      <span class="type ${type0}">${type0}</span>
-      </div>
-      </div>`
-      )
-  }
+  arrayPokemons[data.id].gerarType1()
+
+  generatorSingleCard(data.id, main.innerHTML)
   
 }
-async function generatorCard(){
-  
-  for(let i = 1; i<=151 ; i++){
-    await fetchPokemon(i)
+
+function generatorSingleCard(id, add=''){
+  if(arrayPokemons[id].type1 != undefined){
+
+    main.innerHTML = add + (
+    `<div class="pokeCard card${arrayPokemons[id].type0}">
+    <div class="infos">
+    <p>${arrayPokemons[id].id}. ${arrayPokemons[id].name}</p>
+      <img src="${arrayPokemons[id].img}" alt="${arrayPokemons[id].name}">
+    </div>
+    <div class="divTypes">
+    <span class="type ${arrayPokemons[id].type0}">${arrayPokemons[id].type0}</span>
+    <span class="type ${arrayPokemons[id].type1}">${arrayPokemons[id].type1}</span>
+    </div>
+    </div>`
+    )
+
+  }else{
+    main.innerHTML = add + (
+    `<div id="app" class="pokeCard card${arrayPokemons[id].type0}">
+    <div class="infos">
+    <p>${arrayPokemons[id].id}. ${arrayPokemons[id].name}</p>
+      <img src="${arrayPokemons[id].img}" alt="${arrayPokemons[id].name}">
+    </div>
+    <div class="divTypes">
+    <span class="type ${arrayPokemons[id].type0}">${arrayPokemons[id].type0}</span>
+    </div>
+    </div>`
+    )
   }
+}
+async function generatorCard(){
+
+  main.innerHTML = ''
+  
+  for(let i = 1; i<=4 ; i++){
+    await fetchPokemon(i) 
+  }
+
+
+  input.removeAttribute('disabled', 'disabled')
+  input.placeholder = 'Type the pokemon name or id'
 
 }
 
 generatorCard()
 
-function searchPokemon(name){
-  const input = document.querySelector('input#search')
+input.addEventListener('keyup', _.debounce(searchPokemon, 800))
 
-  if(input.value.length == 0){
-    generatorCard(151)  
+function searchPokemon(){
+  const rawText = input.value
+  var text = rawText.toString().toLowerCase() 
+  
+  if(rawText.length == 0){
+    generatorCard()
   }
   else{
-      
+    main.value = ''
+    const searchResult = arrayPokemons.filter((element) => {
+      element.name = '' + element.name
+      element.name.toString()
+      return element.name.toLowerCase().includes(text) == true
+    })
+    console.log(searchResult)
+    if(searchResult[0]){
+      for(let i = 0; i > searchResult.length ; i++){
+        generatorSingleCard(searchResult[i].id)
+      }
+    }else{
+      main. innerHTML = '<p class="noresult">nada encontrado</p>'
+    }
   }
+
 }
